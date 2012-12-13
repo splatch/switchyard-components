97cfdc297cab9ab4f8d675effb1ef5148ae06a8c
Lukasz Dywicki <luke@code-house.org>
2012-12-13 15:22:25 +0100

    Move AtomComponent to proper package
    Signed-off-by: Lukasz Dywicki <luke@code-house.org>

--- camel/camel-atom/src/main/java/org/switchyard/component/camel/atom/deploy/CamelAtomComponent.java
+++ camel/camel-atom/src/main/java/org/switchyard/component/camel/atom/deploy/CamelAtomComponent.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
  * MA  02110-1301, USA.
  */
-package org.switchyard.component.camel.deploy;
+package org.switchyard.component.camel.atom.deploy;
 
 import org.switchyard.component.camel.atom.model.v1.V1CamelAtomBindingModel;
 import org.switchyard.component.camel.common.deploy.BaseBindingComponent;
